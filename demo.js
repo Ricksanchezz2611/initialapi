757c635fc1de73dcd68f9e417fbed52df09ad805
karthikeya <kalurikarthikeya2611@gmail.com>
2025-03-11 15:41:34 +0530

    upadted

--- demo.js
+++ demo.js
@@ -1,6 +1,41 @@
 const express = require("express");
-const app = express();
-app.use(express.static("public"));
-app.listen(3000, () => {
-    console.log("Server is running on port 3000");
+
+const port = 3000;
+const morgan = require("morgan");
+const helmet = require("helmet");
+const cors = require("cors");
+const rateLimit = require("express-rate-limit");
+
+
+const app = express();;
+
+//middleware
+app.use(morgan("dev"));
+app.use(helmet());
+app.use(cors());
+app.use(express.json());
+
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000,  // ✅ Corrected
+    max : 100,
+});
+
+app.use(limiter);
+
+app.get("/", (req,res) => {
+    res.json({
+        message: "Welcome to the scalable API! 🚀",
+    });
+})
+
+app.use((err,req,res,next) => {
+    res.status(500).json({
+        message: err.message,
+    });
+});
+
+const PORT = 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT} 🚀`);
 });
